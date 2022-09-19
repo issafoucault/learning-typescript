dcb7219b4305402639edc3a87942c0ca6c72ee70
Alejandro Foucault <afoucault@internal.issprops.com>
2022-09-19 11:11:25 -0700

    CHAPTER 2 - The Type System - Part 1

---
+++ ch-02.js
@@ -0,0 +1,4 @@
+var bestSong = Math.random() > 0.5
+    ? "Chain of Fools"
+    : "Respect";
+console.log('test');
---
+++ ch-02.ts
@@ -0,0 +1,17 @@
+let bestSong = Math.random() > 0.5
+  ? "Chain of Fools"
+  : "Respect";
+
+// let firstName = "Whitney";
+//firstName.length();
+//
+// This expression is not callable.
+//   Type 'Number' has no call signatures.
+
+// Assignability
+let firstName = "Carole";
+firstName = "Patricia";
+
+let lastName = "King";
+// lastName = true;
+// Error: Type 'boolean' is not assignable to type 'string'.
