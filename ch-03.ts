6e52314037442f22419d88ee3f578223a17cb8e7
Alejandro Foucault <afoucault@internal.issprops.com>
2022-09-23 13:30:39 -0700

    Ch3 Unions and Literals Part 2

--- ch-03.ts
+++ ch-03.ts
@@ -31,3 +31,60 @@ admiral = "Grace Hopper";
 admiral.toUpperCase(); // Ok: string
 
 admiral.toFixed(); // Error: property 'to fixed' does not exist on type 'string'
+
+// Conditional Checks
+
+// Type of scientist: number | string
+let scientist = Math.random() > 0.5
+  ? "Rosalind Franklin"
+  : 51;
+
+if (scientist === "Rosalind Franklin") {
+  // Type of scientist: string
+  scientist.toUpperCase(); // Ok
+}
+
+// Type of scientist: number | string
+scientist.toUpperCase();
+// ~~~~~~~~~~~
+// Error: Property 'toUpperCase' does not exist on type 'string | number'.
+// Property 'toUpperCase' does not exist on type 'number'.
+
+// Typeof Checks
+
+let researcher = Math.random() > 0.5
+  ? "Rosalind Franklin"
+  : 51;
+
+if (typeof researcher === "string") {
+  researcher.toUpperCase(); // Ok: string
+}
+
+if (!(typeof researcher === "string")) {
+  researcher.toFixed(); // Ok: number
+} else {
+  researcher.toUpperCase(); // Ok: string
+}
+
+typeof researcher === "string"
+  ? researcher.toUpperCase() // Ok: string
+  : researcher.toFixed(); // Ok: number
+
+// Literal types
+const philosopher = "Hypatia";
+
+// Literal Assignability
+
+let specificallyAda: "Ada";
+
+specificallyAda = "Ada"; // Ok
+
+specificallyAda = "Byron";
+// Error: Type 'Byron' is not assignble to type 'Ada'
+
+let someString = ""; // Type: string
+
+specificallyAda = someString;
+// Error: Type 'string' is not assignable to type '"Ada"'.
+
+someString = ":)";
