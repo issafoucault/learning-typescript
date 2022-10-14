94f1cbd2a31795d9cc8cdd7aa4c088d0c3876ddc
Alejandro Foucault <afoucault@internal.issprops.com>
2022-10-14 09:13:55 -0700

    Ch4 Objects Part 1

--- ch-04.ts
+++ ch-04.ts
@@ -41,3 +41,52 @@ poetLater = {
 };
 
 poetLater = "Emily Dickinson";
+
+// Structural Typing
+
+type WithFirstName = {
+  firstName: string;
+};
+
+type WithLastName = {
+  lastName: string;
+};
+
+const hasBoth = {
+  firstName: "Lucille",
+  lastName: "Clifton",
+};
+
+// Ok: `hasBoth` contains a `firstName` property of type `string`
+let withFirstName: WithFirstName = hasBoth;
+
+// Ok: `hasBoth` contains a `lastName` property of type `string`
+let withLastName: WithLastName = hasBoth
+
+// Usage Checking
+
+type FirstAndLastNames = {
+  first: string;
+  last: string;
+}
+
+// Ok
+const hasBoth: FirstAndLastNames = {
+  first: "Sarojini",
+  last: "Naidu",
+};
+
+const hasOnlyOne: FirstAndLastNames = {
+  first: "Sappho"
+};
+// Property 'last' is missing in type '{ first: string; }'
+// but required in type 'FirstAndLastNames'.
+
+type TimeRange = {
+  start: Date;
+};
+
+const hasStartString: TimeRange = {
+  start: "1879-02-13",
+  // Error: Type 'string' is not assignable to type 'Date'.
+};
