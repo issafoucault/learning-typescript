4a659fb06995f77cb2b71d09f26e0218bbe56cd3
Alejandro Foucault <afoucault@internal.issprops.com>
2022-10-21 15:47:12 -0700

    Ch4 Objects Part 3

--- ch-04.ts
+++ ch-04.ts
@@ -10,6 +10,7 @@ poet.name;
 
 poet.end;
 
+
 // Declaring Object Types
 
 let poetLater: {
@@ -24,6 +25,7 @@ poetLater = {
 };
 
 poetLater = "Sappho";
+
 
 // Aliased Object Types
 
@@ -41,6 +43,7 @@ poetLater = {
 };
 
 poetLater = "Emily Dickinson";
+
 
 // Structural Typing
 
@@ -62,6 +65,7 @@ let withFirstName: WithFirstName = hasBoth;
 
 // Ok: `hasBoth` contains a `lastName` property of type `string`
 let withLastName: WithLastName = hasBoth
+
 
 // Usage Checking
 
@@ -90,3 +94,119 @@ const hasStartString: TimeRange = {
   start: "1879-02-13",
   // Error: Type 'string' is not assignable to type 'Date'.
 };
+
+
+// Excess Property Checking
+
+type Poet = {
+  born: number;
+  name: string;
+}
+
+// Ok: all fields match what's expected in Poet
+const poetMatch : Poet = {
+  born: 1928,
+  name: "Maya Angelou"
+}
+
+const extraProperty: Poet = {
+  activity: "walking",
+  born: 1935,
+  name: "Mary Oliver",
+}
+// Error: Type '{ activity: string; born: number; name: string; }'
+// is not assignable to type 'Poet'.
+// Object literal may only specify known properties,
+// and 'activity' does not exist in type 'Poet'.
+
+const existingObject = {
+  activity: "walking",
+  born: 1935,
+  name: "Mary Oliver",
+};
+
+const extraPropertyButOk: Poet = existingObject; // Ok
+
+
+// Nested Object Types
+type Poem = {
+  author: {
+    firstName: string;
+    lastName: string;
+  };
+  name: string;
+};
+
+// Ok
+const poemMatch: Poem = {
+  author: {
+    firstName: "Sylvia",
+    lastName: "Plath",
+  },
+  name: "Lady Lazarus",
+};
+
+const poemMismatch: Poem = {
+  author: {
+    name: "Sylvia Plath",
+  },
+  // Error: Type '{ name: string; }' is not assignable
+  // to type '{ firstName: string; lastName: string; }'.
+  // Object literal may only specify known properties, and 'name'
+  // does not exist in type '{ firstName: string; lastName: string; }'.
+  name: "Tulips",
+};
+
+type Author = {
+  firstName: string;
+  lastName: string;
+};
+
+type Poem = {
+  author: Author;
+  name: string;
+};
+
+const poemMismatch: Poem = {
+  author: {
+    name: "Sylvia Plath",
+  },
+  // Error: Type '{ name: string; }' is not assignable to type 'Author'.
+  // Object literal may only specify known properties,
+  // and 'name' does not exist in type 'Author'.
+  name: "Tulips",
+};
+
+
+// Optional Properties
+type Book = {
+  author?: string;
+  pages: number;
+};
+
+// Ok
+const ok: Book = {
+  author: "Rita Dove",
+  pages: 80,
+};
+
+const missing: Book = {
+  author: "Rita Dove",
+};
+// Error: Property 'pages' is missing in type
+// '{ author: string; }' but required in type 'Book'.
+
+type Writers = {
+  author: string | undefined;
+  editor ?: string;
+};
+
+// Ok: author is provided as undefined
+const hasRequired: Writers = {
+  author: undefined,
+};
+
+const missingRequired: Writers = {};
+// ~~~~~~~~~~~~~~~
+// Error: Property 'author' is missing in type
+// '{}' but required in type 'Writers'.
